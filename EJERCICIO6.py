b2cef89f861691874edebf9830cd7a41ab963ea8
maximilianofer <163897917+asistenciasocial@users.noreply.github.com>
2024-05-13 15:22:02 -0300

    PYTHON 13/05

---
+++ EJERCICIO6.py
@@ -0,0 +1,7 @@
+def Ejercicio6():
+    ladoA = int(input("Cuánto mide el lado a?"))
+    ladoB = int(input("Cuánto mide el lado b?"))
+
+
+    suptriangulo = ladoA * ladoB /2
+    print("La superficie del triángulo es: "+suptriangulo)
---
+++ EJERCICIO8.py
@@ -0,0 +1,8 @@
+def Ejercicio8():
+    n1 = int(input("Ingrese un número."))
+    n2 = int(input("Ingrese otro número."))
+
+    NUM1 = n1/100
+    porcentaje = n2 / NUM1
+
+    print("El promedio es:%", porcentaje)
